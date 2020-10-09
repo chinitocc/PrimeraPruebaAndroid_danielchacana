47d024798b5f85cb948ecf3d244e4dd9ecd1b9f9
daniel <danielcl342@gmail.com>
2020-10-09 13:07:47 -0300

    Primera prueba de programacion en android

---
+++ app/src/main/java/cl/santotomas/creditcardform/CreditCard.java
@@ -0,0 +1,14 @@
+package cl.santotomas.creditcardform;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreditCard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credit_card);
+    }
+}
